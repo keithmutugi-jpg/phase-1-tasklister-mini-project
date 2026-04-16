d6c4cb278033055cc576aa0f447ff3a34104d7ec
Keith Mutugi <keithmutugi64@gmail.com>
2026-04-16 15:36:45 +0300

    commit changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// your code here
+const form = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#tasks');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const taskDescription = document.querySelector('#new-task-description').value;
+  
+  if (taskDescription.trim() !== '') {
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskDescription;
+    taskList.appendChild(taskItem);
+    
+    // Clear the input field
+    document.querySelector('#new-task-description').value = '';
+  }
 });
